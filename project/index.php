94d2ca2efe58257103cc778039cd440cc6381bf3
Pablo Muralles <pablomuralles28@gmail.com>
2022-11-01 13:15:13 -0600

    index page

---
+++ project/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <title>Inscripciones Eventos CDAG</title>
+</head>
+<body>
+    <center> <h1>Eventos CDAG<h1></center>
+    <p>Bienvenidos al sistema de la CDAG para el control de los eventos impartidos</p>
+    <p>Selecciones la opción que desea:</p>
+
+    <input type="button" value="Crear Evento" class="create_event_button" data-inline="true" id="btn_create_event" onClick="document.location.href='validation-cui.html'" />
+
+    <input type="button" value="Eliminar Evento" class="create_event_button"  data-inline="true" id="btn_create_event" onClick="document.location.href='validation-cui.html'" />
+   
+    <input type="button" value="Lista de evento" class="create_event_button"  data-inline="true" id="btn_create_event" onClick="document.location.href='validation-cui.html'" />
+
+    <input type="button" value="Lista de evento" class="create_event_button"  data-inline="true" id="btn_create_event" onClick="document.location.href='validation-cui.html'" />
+
+
+
+</body>
+</html>
